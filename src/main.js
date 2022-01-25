45806a3d173b235197883b4654b98e5d0bf309e6
fenghailin <1021944439@qq.com>
2022-01-25 15:07:11 +0800

    引入 element-plus 引入 normalize.css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
+import '@/utils/css/normalize.css' // A modern alternative to CSS resets
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(ElementUI).mount('#app')
